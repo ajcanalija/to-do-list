1d93342c74e18aff3916f493cb64830ec569b5ea
ajcanalija <59231901+ajcanalija@users.noreply.github.com>
2022-08-08 13:30:15 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+//function for creating list items with button
+function createListElement(){
+	var div=document.createElement("div");
+	//var checkBox=document.createElement("input[type=checkbox]");
+	var li= document.createElement("li");
+	var delBtn=document.createElement("button");
+	div.classList.add("tasklist");
+	ul.appendChild(div);
+	div.append(li, delBtn);
+	li.classList.add("task");
+	li.appendChild(document.createTextNode(input.value));
+	input.value = "";
+	delBtn.classList.add("del-btn");
+	delBtn.innerHTML='<i class="fa fa-close"></i>';
+}
+
+function inputLength() {
+	return input.value.length;
+}
+
+//Function to Add Lists on Click 
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+//Function Add Lists on Keypress
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+
+button.addEventListener("click", addListAfterClick);  //Add Lists on Click | EVENT
+
+input.addEventListener("keypress", addListAfterKeypress); //Add Lists on Keypress |EVENT
+
+// Task is Done
+
+function done(task){
+	if(task.target.tagName==="LI"){
+		task.ta
+	}
+}
