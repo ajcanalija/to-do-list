f0ed5a4a98e3f5929cc46464649fc1ec568b29aa
ajcanalija <59231901+ajcanalija@users.noreply.github.com>
2022-09-09 02:59:56 +0800

    check box
    Able to add checkbox when creating a task.

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var complete = document.getElementById("complete")
 
 //function for creating list items with button
 function createListElement(){
 	var div=document.createElement("div");
 	//var checkBox=document.createElement("input[type=checkbox]");
+	var check = document.createElement("INPUT");
+	check.setAttribute("type","checkbox");
 	var li= document.createElement("li");
-	var delBtn=document.createElement("button");
+	var delBtn=document.createElement("button");	
 	div.classList.add("tasklist");
 	ul.appendChild(div);
-	div.append(li, delBtn);
+	div.append(check,li, delBtn);
 	li.classList.add("task");
 	li.appendChild(document.createTextNode(input.value));
 	input.value = "";
 	delBtn.classList.add("del-btn");
-	delBtn.innerHTML='<i class="fa fa-close"></i>';
+	delBtn.innerHTML='Del';
 }
 
 function inputLength() {
@@ -38,17 +41,20 @@ function addListAfterKeypress(event) {
 
 // Task is Done
 
+//Strikesthrough tasks when clicked
 function done(task){
 	if(task.target.tagName==="LI"){
 		task.target.classList.toggle("done");
 	}
 }
 
+//Deletes a task when "del" button is clicked
 function deleteTask(element){
 	if(element.target.className==="del-btn"){
 		element.target.parentElement.remove();
 	}
 }
+
 
 function deleteTaskClick(element){
 	done(element);
